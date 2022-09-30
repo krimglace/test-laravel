71b18e846a7d51056a9a47e062de237f0122aaa3
krimglace <78518413+krimglace@users.noreply.github.com>
2022-09-30 17:01:30 +0700

    Add files via upload

---
+++ database/migrations/2022_09_29_144519_create_produks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->increments('id_produk');
+            $table->string('nama_produk');
+            $table->string('harga_produk');
+            $table->integer('jumlah_produk');
+            $table->integer('id_variasi');
+            $table->integer('id_kategori');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+};
---
+++ database/migrations/2022_09_29_144616_create_variasis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variasi', function (Blueprint $table) {
+            $table->increments('id_variasi');
+            $table->string('nama_variasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variasis');
+    }
+};
